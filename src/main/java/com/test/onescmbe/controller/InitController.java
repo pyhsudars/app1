45e89c5d95f2bd08b715b003ac43affb88cf5ba1
pyhsudars <mail2harishsudarsanan@gmail.com>
2023-01-22 15:37:07 +0530

    Update InitController.java

--- src/main/java/com/test/onescmbe/controller/InitController.java
+++ src/main/java/com/test/onescmbe/controller/InitController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8081"})
+@CrossOrigin
 @RestController
 public class InitController {
 	
